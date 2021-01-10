9068bad5b9004425f669cf11dad31ac258fec305
Jake Ball <jacobjball18@gmail.com>
2021-01-10 14:47:32 -0700

    added react client side

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+
+  resources :administrators do
+    resources :interviewers do
+      resources :interviewees do
+      end
+    end
+  end
+  resources :assessments do
+    resources :assessment_questions do
+      resources :assessment_answers do
+      end
+    end
+  end
+end
 end
