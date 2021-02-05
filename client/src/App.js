12f44069a7ea4401353d7aa7d1183c0560bb5b7c
Jake Ball <jacobjball18@gmail.com>
2021-02-05 12:15:33 -0700

    cant run a rails s says my gemfile doesnt contain devise

--- client/src/App.js
+++ client/src/App.js
@@ -8,9 +8,9 @@ const App = () => (
   <>
     <Container>
       <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
       </BrowserRouter>
     </Container>
   </>
