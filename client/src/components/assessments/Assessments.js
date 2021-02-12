ff5b8728bb6cd648b9cb2620e7dc065c9d19e465
Jake Ball <jacobjball18@gmail.com>
2021-02-12 12:28:07 -0700

    revert back to old code

--- client/src/components/assessments/Assessments.js
+++ client/src/components/assessments/Assessments.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import { Button, Icon } from 'semantic-ui-react';
+// import React from 'react';
+// import { Button, Icon } from 'semantic-ui-react';
 
-const Assessments = ({ id, updateAssessment, deleteAssessment }) => (
-  <div style={styles.flex}>
-   <div style={styles.flex}>
-     <Button
-       onClick={() => updateAssessment(id)}
-     />
-   </div>
-   <Button
-     onClick={() => deleteAssessment(id)}
-   >
-     <Icon name="trash" />
-   </Button>
- </div>
-)
+// const Assessments = ({ id, updateAssessment, deleteAssessment }) => (
+//   <div style={styles.flex}>
+//    <div style={styles.flex}>
+//      <Button
+//        onClick={() => updateAssessment(id)}
+//      />
+//    </div>
+//    <Button
+     
+//      onClick={() => deleteAssessment(id)}
+//      style={{ marginLeft: "15px", }}
+//    >
+//      <Icon name="trash" />
+//    </Button>
+//  </div>
+// )
 
 
-export default Assessments;
+// export default Assessments;
